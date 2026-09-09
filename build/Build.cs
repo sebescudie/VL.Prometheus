752cf77412d7e07da9769a32ab71d1c6aea751e5
sebescudie <accounts@sebescudie.fr>
2026-09-09 18:33:46 +0200

    Updates Fallout pipeline

--- build/Build.cs
+++ build/Build.cs
@@ -9,29 +9,64 @@ using Fallout.Common.Tooling;
 using Fallout.Common.Utilities.Collections;
 using static Fallout.Common.EnvironmentInfo;
 using static Fallout.Common.IO.PathConstruction;
+using Fallout.Common.Tools.NuGet;
+using System.IO;
+using Fallout.Common.Tools.Git;
 
 class Build : FalloutBuild
 {
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Clean);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+    AbsolutePath NuspecFile => RootDirectory / "deployment/VL.Prometheus.nuspec";
+    AbsolutePath VersionFile => RootDirectory / "version.txt";
+
+    [Parameter("Nuget feed URL")]
+    readonly string Source = "https://api.nuget.org/v3/index.json";
+
+    [Parameter("Nuget feed API Key")]
+    [Secret]
+    readonly string ApiKey;
+
+    string Version => File.ReadAllText(VersionFile).Trim();
 
     Target Clean => _ => _
-        .Before(Restore)
+        .Before()
         .Executes(() =>
         {
+            OutputDirectory.CreateOrCleanDirectory();
         });
 
-    Target Restore => _ => _
+    Target Pack => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
         {
+            NuGetTasks.NuGetPack(f => f
+                .SetTargetPath(NuspecFile)
+                .SetVersion(Version)
+                .SetOutputDirectory(OutputDirectory));
         });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
+    Target Push => _ => _
+        .DependsOn(Pack)
         .Executes(() =>
         {
+            NuGetTasks.NuGetPush(p => p
+                .SetSource(Source)
+                .SetApiKey(ApiKey));
         });
 
+    Target Tag => _ => _
+        .Executes(() =>
+        {
+            GitTasks.Git($"tag -a {Version} -m \"VL Prometheus {Version}\"");
+        });
+
+    Target Release => _ => _
+    .DependsOn(Tag, Push)
+    .Executes(() =>
+    {
+        // Meta target, only calls Tag and Push, which themselves
+        // make the whole pipeline run
+    });
 }
